7755612a40c03272910d0a09996ac23a95af2152
DarthNixilis <65317550+DarthNixilis@users.noreply.github.com>
2025-08-25 23:39:24 -0700

    script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- DATA FETCHING ---
     async function loadCardDatabase() {
         try {
-            const response = await fetch(`./cardDatabase.json?v=${new Date().getTime()}`);
+            const response = await fetch(`./cardDatabase.json`);
             if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
             cardDatabase = await response.json();
             initializeApp();
